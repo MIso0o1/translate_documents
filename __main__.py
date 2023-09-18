a2b9699b0e84d21bdf6fbddaa21493fd5eac5c97
Michal Somsky <89775059+MIso0o1@users.noreply.github.com>
2023-09-18 19:15:04 +0200

    fix

--- __main__.py
+++ __main__.py
@@ -40,4 +40,6 @@ def translate():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use the PORT environment variable if available (for Heroku)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
